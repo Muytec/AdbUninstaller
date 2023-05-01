f5ba8df994314b3ebff02b6163faef0d35d08af6
God A <2418571896@qq.com>
2023-05-01 22:27:04 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace AdbUninstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            statusText = new Label();
+            statusButton = new Button();
+            uninstallButton = new Button();
+            installButton = new Button();
+            searchBox = new TextBox();
+            logBox = new RichTextBox();
+            appListView = new ListView();
+            RefreshAppListbtn = new Button();
+            groupLabel = new LinkLabel();
+            githubLabel = new LinkLabel();
+            AuthorLabel = new LinkLabel();
+            SuspendLayout();
+            // 
+            // statusText
+            // 
+            statusText.AutoSize = true;
+            statusText.Location = new Point(12, 847);
+            statusText.Name = "statusText";
+            statusText.Size = new Size(82, 24);
+            statusText.TabIndex = 0;
+            statusText.Text = "连接状态";
+            // 
+            // statusButton
+            // 
+            statusButton.Location = new Point(901, 783);
+            statusButton.Name = "statusButton";
+            statusButton.Size = new Size(178, 48);
+            statusButton.TabIndex = 1;
+            statusButton.Text = "连接状态检测";
+            statusButton.UseVisualStyleBackColor = true;
+            statusButton.Click += statusButton_Click;
+            // 
+            // uninstallButton
+            // 
+            uninstallButton.BackColor = Color.Brown;
+            uninstallButton.Font = new Font("Microsoft YaHei UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            uninstallButton.ForeColor = SystemColors.ButtonFace;
+            uninstallButton.Location = new Point(1303, 783);
+            uninstallButton.Name = "uninstallButton";
+            uninstallButton.Size = new Size(139, 49);
+            uninstallButton.TabIndex = 2;
+            uninstallButton.Text = "卸载选中";
+            uninstallButton.UseVisualStyleBackColor = false;
+            uninstallButton.Click += uninstallButton_Click;
+            // 
+            // installButton
+            // 
+            installButton.Location = new Point(1466, 784);
+            installButton.Name = "installButton";
+            installButton.Size = new Size(132, 48);
+            installButton.TabIndex = 3;
+            installButton.Text = "安装应用";
+            installButton.UseVisualStyleBackColor = true;
+            installButton.Click += installButton_Click;
+            // 
+            // searchBox
+            // 
+            searchBox.ForeColor = Color.DimGray;
+            searchBox.Location = new Point(901, 738);
+            searchBox.Multiline = true;
+            searchBox.Name = "searchBox";
+            searchBox.Size = new Size(697, 30);
+            searchBox.TabIndex = 5;
+            searchBox.Text = "在此处搜索，回车返回搜索结果";
+            searchBox.TextChanged += searchBox_TextChanged;
+            // 
+            // logBox
+            // 
+            logBox.Location = new Point(901, 12);
+            logBox.Name = "logBox";
+            logBox.ScrollBars = RichTextBoxScrollBars.ForcedBoth;
+            logBox.Size = new Size(697, 711);
+            logBox.TabIndex = 6;
+            logBox.Text = "";
+            // 
+            // appListView
+            // 
+            appListView.CheckBoxes = true;
+            appListView.Location = new Point(12, 12);
+            appListView.Name = "appListView";
+            appListView.Size = new Size(863, 819);
+            appListView.TabIndex = 7;
+            appListView.UseCompatibleStateImageBehavior = false;
+            appListView.View = View.Details;
+            // 
+            // RefreshAppListbtn
+            // 
+            RefreshAppListbtn.Location = new Point(1103, 783);
+            RefreshAppListbtn.Name = "RefreshAppListbtn";
+            RefreshAppListbtn.Size = new Size(172, 48);
+            RefreshAppListbtn.TabIndex = 8;
+            RefreshAppListbtn.Text = "刷新应用列表";
+            RefreshAppListbtn.UseVisualStyleBackColor = true;
+            RefreshAppListbtn.Click += RefreshAppListbtn_Click;
+            // 
+            // groupLabel
+            // 
+            groupLabel.AutoSize = true;
+            groupLabel.Location = new Point(1378, 856);
+            groupLabel.Name = "groupLabel";
+            groupLabel.Size = new Size(100, 24);
+            groupLabel.TabIndex = 9;
+            groupLabel.TabStop = true;
+            groupLabel.Text = "加入交流群";
+            groupLabel.LinkClicked += groupLabel_LinkClicked;
+            // 
+            // githubLabel
+            // 
+            githubLabel.AutoSize = true;
+            githubLabel.Location = new Point(1498, 856);
+            githubLabel.Name = "githubLabel";
+            githubLabel.Size = new Size(100, 24);
+            githubLabel.TabIndex = 10;
+            githubLabel.TabStop = true;
+            githubLabel.Text = "获取最新版";
+            githubLabel.LinkClicked += githubLabel_LinkClicked;
+            // 
+            // AuthorLabel
+            // 
+            AuthorLabel.AutoSize = true;
+            AuthorLabel.Location = new Point(1275, 856);
+            AuthorLabel.Name = "AuthorLabel";
+            AuthorLabel.Size = new Size(82, 24);
+            AuthorLabel.TabIndex = 11;
+            AuthorLabel.TabStop = true;
+            AuthorLabel.Text = "联系作者";
+            AuthorLabel.LinkClicked += AuthorLabel_LinkClicked;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1623, 896);
+            Controls.Add(AuthorLabel);
+            Controls.Add(githubLabel);
+            Controls.Add(groupLabel);
+            Controls.Add(RefreshAppListbtn);
+            Controls.Add(appListView);
+            Controls.Add(logBox);
+            Controls.Add(searchBox);
+            Controls.Add(installButton);
+            Controls.Add(uninstallButton);
+            Controls.Add(statusButton);
+            Controls.Add(statusText);
+            Name = "Form1";
+            Text = "ADBUninstaller v0.1.0";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label statusText;
+        private Button statusButton;
+        private Button uninstallButton;
+        private Button installButton;
+        private TextBox searchBox;
+        private RichTextBox logBox;
+        private ListView appListView;
+        private Button RefreshAppListbtn;
+        private LinkLabel groupLabel;
+        private LinkLabel githubLabel;
+        private LinkLabel AuthorLabel;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,431 @@
+using AdvancedSharpAdbClient;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Windows.Forms;
+
+namespace AdbUninstaller
+{
+    public partial class Form1 : Form
+    {
+        private readonly AdbClient _adbClient = new AdbClient();
+        private readonly DeviceData _device;
+        private List<PackageInfo> appList = new List<PackageInfo>();
+        private List<PackageInfo> filterList = new List<PackageInfo>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var devices = _adbClient.GetDevices();
+            if (!devices.Any())
+            {
+                MessageBox.Show("未连接任何设备", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+            _device = devices[0];
+
+            UpdateDeviceStatus();
+            RefreshAppList();
+        }
+
+        private void UpdateDeviceStatus()
+        {
+            var deviceOnline = IsDeviceOnline();
+            if (!deviceOnline)
+            {
+                statusText.Text = "设备未连接";
+                statusText.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+
+            var model = ExecuteShellCommand("getprop ro.product.model").TrimEnd('\r', '\n');
+            var serial = ExecuteShellCommand("getprop ro.serialno").TrimEnd('\r', '\n');
+            statusText.Text = $"设备已连接：{model} ({serial})";
+            statusText.ForeColor = System.Drawing.Color.Green;
+        }
+
+        private bool IsDeviceOnline()
+        {
+            var devices = _adbClient.GetDevices();
+            foreach (var device in devices)
+            {
+                if (device.State == DeviceState.Online)
+                {
+                    return true; // 返回设备是否在线
+                }
+            }
+            return false;
+        }
+
+        //应用列表实时刷新
+        private delegate void RefreshAppListDelegate();
+
+        private async void RefreshAppList()
+        {
+            Log("正在刷新应用列表...");
+
+            // 异步获取应用列表
+            var packageTask = Task.Run(() =>
+            {
+                appList.Clear();
+                var packages = ExecuteShellCommand("pm list packages -f");
+                if (string.IsNullOrEmpty(packages))
+                {
+                    Log("获取应用列表失败");
+                    return;
+                }
+
+                var packageLines = packages.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var line in packageLines)
+                {
+                    var pair = line.Split('=');
+
+                    var packagePath = pair[0].Replace("package:", "");
+                    var packageName = pair[1];
+
+                    string appType;
+                    if (packagePath.StartsWith("/system/app"))
+                    {
+                        appType = "系统应用";
+                    }
+                    else if (packagePath.StartsWith("/system/priv-app"))
+                    {
+                        appType = "系统特权应用";
+                    }
+                    else
+                    {
+                        appType = "用户应用";
+                    }
+
+                    var appInfoJson = ExecuteShellCommand($"dumpsys package {packagePath}");
+                    var appInfo = JsonConvert.DeserializeObject<AppInfo>(appInfoJson);
+                    var appName = appInfo?.Application?.Label ?? packagePath;
+
+                    appList.Add(new PackageInfo
+                    {
+                        PackageName = packageName,
+                        AppName = appName,
+                        PackagePath = packagePath,
+                        AppType = appType
+                    });
+                }
+
+                appList = appList.OrderByDescending(p => p.AppType).ToList();
+                filterList = appList;
+            });
+
+            await packageTask;
+
+            // 更新显示列表和记录日志
+            BeginInvoke(new RefreshAppListDelegate(() =>
+            {
+                ShowAppList();
+                Log($"应用列表已经刷新，共有 {appList.Count} 个应用");
+            }));
+        }
+
+
+        private void FilterAppList(string keyword)
+        {
+            if (string.IsNullOrEmpty(keyword))
+            {
+                filterList = appList;
+            }
+            else
+            {
+                filterList = appList.Where(p => p.PackageName.ToLower().Contains(keyword.ToLower())).ToList();
+            }
+            ShowAppList();
+        }
+
+        private void ShowAppList()
+        {
+            appListView.Items.Clear();
+            filterList.ForEach(p =>
+            {
+                var item = new ListViewItem(new string[] { p.AppType, p.AppName, p.PackageName });
+                appListView.Items.Add(item);
+            });
+        }
+        //异步卸载操作
+
+        private async Task UninstallSelectedAppsAsync()
+        {
+            Log($"卸载操作开始...");
+            try
+            {
+                var selectedItems = appListView.CheckedItems;
+                if (selectedItems.Count == 0)
+                {
+                    MessageBox.Show("请至少选中一个应用", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Log($"未选中任何应用，卸载操作终止。");
+                    return;
+                }
+                foreach (ListViewItem item in selectedItems)
+                {
+                    var packageName = item.SubItems[2].Text;
+                    if (!string.IsNullOrEmpty(packageName))
+                    {
+                        Log($"包 {packageName} 正在卸载中...");
+                        var result = await Task.Run(() => ExecuteShellCommand($"pm uninstall --user 0 {packageName}"));
+                        if (result.Contains("Success"))
+                        {
+                            Log($"{item.SubItems[1].Text} 已卸载");
+                        }
+                        else
+                        {
+                            Log($"卸载失败：{result}");
+                        }
+                    }
+                }
+                Log($"卸载操作结束。");
+                RefreshAppList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Log($"卸载操作失败：{ex.Message}");
+            }
+        }
+
+
+
+
+
+        private async Task InstallApkFilesAsync(string[] apkFiles)
+        {
+            OnOperationStarted("安装操作开始...");
+
+            foreach (var filePath in apkFiles)
+            {
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+                        Log($"APK {filePath} 正在安装中...");
+                        var result = await Task.Run(() =>
+                        {
+                            // 执行安装命令
+                            var arguments = $"install \"{filePath}\"";
+                            return ExecuteCommand("adb", arguments);
+                        });
+
+                        if (result.Contains("Success"))
+                        {
+                            Log($"APK {filePath} 安装成功");
+                        }
+                        else
+                        {
+                            Log($"安装失败：{result}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Log($"安装失败：{ex.Message}");
+                    }
+                }
+                else
+                {
+                    Log($"文件不存在：{filePath}");
+                }
+            }
+            OnOperationFinished("安装操作结束。");
+            RefreshAppList();
+        }
+        public event EventHandler<string> OperationStarted;
+        public event EventHandler<string> OperationFinished;
+
+        private void OnOperationStarted(string message)
+        {
+            OperationStarted?.Invoke(this, message);
+        }
+
+        private void OnOperationFinished(string message)
+        {
+            OperationFinished?.Invoke(this, message);
+        }
+
+
+
+
+
+
+
+
+        private string ExecuteCommand(string fileName, string arguments)
+        {
+            string result = null;
+            var psi = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            using (var process = new Process())
+            {
+                process.StartInfo = psi;
+
+                process.Start();
+
+                result = process.StandardOutput.ReadToEnd();
+
+                process.WaitForExit();
+            }
+
+            return result;
+        }
+
+        private string ExecuteShellCommand(string command)
+        {
+            if (!IsDeviceOnline())
+            {
+                // 设备未连接在线，返回空字符串
+                return string.Empty;
+            }
+
+            var receiver = new ConsoleOutputReceiver();
+            try
+            {
+                _adbClient.ExecuteRemoteCommand(command, _device, receiver);
+            }
+            catch (Exception ex)
+            {
+                Log($"执行命令 {command} 失败：{ex.Message}");
+            }
+            return receiver.ToString().TrimEnd('\r', '\n');
+        }
+
+        private void Log(string message)
+        {
+            logBox.AppendText($"{DateTime.Now:HH:mm:ss} {message}\r\n");
+            logBox.ScrollToCaret();
+        }
+
+        private void statusButton_Click(object sender, EventArgs e)
+        {
+            UpdateDeviceStatus();
+        }
+
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+            var keyword = searchBox.Text.Trim();
+            FilterAppList(keyword);
+        }
+
+        private async void uninstallButton_Click(object sender, EventArgs e)
+        {
+            await UninstallSelectedAppsAsync();
+        }
+
+
+        private async void installButton_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "APK 文件|*.apk"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                await InstallApkFilesAsync(openFileDialog.FileNames);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            appListView.Columns.Add("应用类型", 140);
+            appListView.Columns.Add("应用地址", 300);
+            appListView.Columns.Add("包名", 300);
+            appListView.FullRowSelect = true;
+            appListView.MultiSelect = true;
+        }
+
+        private void RefreshAppListbtn_Click(object sender, EventArgs e)
+        {
+            RefreshAppList();
+        }
+
+        private void githubLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenUrl("https://github.com/Muytec/AdbUninstaller/releases");
+        }
+
+        private void groupLabel_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+        {
+            OpenUrl("http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=mGOBfWQd7G0vp7FqVkAocuXyw4fH9XbA&authKey=6bRAnn3ilyhQY2Os1ku6I0glT0%2FFC%2FCIQcNBZ5tAeIHnIfAXDl4iOP8ZunAplR14&noverify=0&group_code=156115036");
+        }
+        private void AuthorLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("微信公众号：慕研" + Environment.NewLine + "QQ:2535093954", "联系作者", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void OpenUrl(string url)
+        {
+            // 获取注册表中的默认浏览器
+
+            try
+            {
+                // 尝试打开默认浏览器并访问链接
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "cmd",
+                    Arguments = "/c start " + url.Replace("\"", ""),
+                    CreateNoWindow = true
+                });
+                return;
+            }
+            catch
+            {
+                Clipboard.SetText(url);
+                MessageBox.Show("无法打开指定网页，链接已复制，请手动打开！");
+            }
+        }
+
+
+    }
+
+    public class AppInfo
+    {
+        [JsonProperty("isSystemApp")]
+        public bool IsSystemApp { get; set; }
+
+        [JsonProperty("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonProperty("versionCode")]
+        public int VersionCode { get; set; }
+
+        [JsonProperty("versionName")]
+        public string VersionName { get; set; }
+
+        [JsonProperty("packageName")]
+        public string PackageName { get; set; }
+
+        [JsonProperty("application")]
+        public ApplicationInfo Application { get; set; }
+    }
+
+    public class ApplicationInfo
+    {
+        [JsonProperty("label")]
+        public string Label { get; set; }
+    }
+
+    public class PackageInfo
+    {
+        public string AppType { get; set; }
+
+        public string AppName { get; set; }
+
+        public string PackageName { get; set; }
+
+        public string PackagePath { get; set; }
+    }
+}
